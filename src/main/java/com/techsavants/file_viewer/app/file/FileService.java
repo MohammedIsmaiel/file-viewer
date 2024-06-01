30e3d7cbd6d2ea8dc321dc26594d8276703ac35e
mohammedismaiel <mohammedismaiel333.z@gmail.com>
2024-06-01 17:29:20 +0300

    enhance fileservice to remove duplicated code

--- src/main/java/com/techsavants/file_viewer/app/file/FileService.java
+++ src/main/java/com/techsavants/file_viewer/app/file/FileService.java
@@ -4,11 +4,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.springframework.stereotype.Service;
+
+import com.techsavants.file_viewer.app.directory.FileInfo;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -16,16 +19,27 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 class FileService {
 
-    public List<String> viewFile(String path) throws IOException {
-        Path filePath = Paths.get(path);
-        return Files.readAllLines(filePath);
+    public FileInfo readFile(String filePath) throws IOException {
+        return readFileFromLine(filePath, 0);
     }
 
-    public List<String> tailFile(String path, int lines) throws IOException {
-        Path filePath = Paths.get(path);
-        try (Stream<String> stream = Files.lines(filePath)) {
-            return stream.skip(Math.max(0, Files.lines(filePath).count() - lines))
-                    .collect(Collectors.toList());
+    public FileInfo readFileFromLine(String filePath, long startLine) throws IOException {
+        log.info("reading file: {} ", filePath);
+        log.info("reading from line : {} ", startLine);
+        Path path = Paths.get(filePath);
+        if (Files.isRegularFile(path)) {
+            BasicFileAttributes attrs = Files.readAttributes(path, BasicFileAttributes.class);
+            FileInfo fileInfo = new FileInfo(path, attrs);
+
+            try (Stream<String> lines = Files.lines(path)) {
+                List<String> contentLines = lines.skip(startLine).collect(Collectors.toList());
+                fileInfo.setContent(String.join("\n", contentLines));
+                fileInfo.setLastLine(startLine + contentLines.size());
+            }
+
+            return fileInfo;
+        } else {
+            throw new IllegalArgumentException("Path is not a regular file");
         }
     }
 }
