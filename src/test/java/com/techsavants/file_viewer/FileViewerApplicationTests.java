cf20de4086a6c4777eb2effd52e2fe807bbdc3df
mohammedismaiel <mohammedismaiel333.z@gmail.com>
2024-06-01 16:16:49 +0300

    added empty test

---
+++ src/test/java/com/techsavants/file_viewer/FileViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techsavants.file_viewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
